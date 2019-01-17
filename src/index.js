9f219c84624ecef51bb8cd4bbad222b057069e9e
Amit Mishra <dev.amitkmishra@gmail.com>
2019-01-17 11:47:03 +0530

    logger middlerware implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -13,7 +13,17 @@ const reducer = combineReducers({
     ctr: counterReducer,
     res: resultReducer
 });
-const store = createStore(reducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('Middleware Req Dispatch', action);
+            const result = next(action);
+            console.log('Middleware next state', store.getState());
+            return result;
+        }
+    }
+}
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
